70ad31d802b6d717a8075a639e475cc71978cfef
Spencer Nelson <s@spenczar.com>
2016-10-02 13:50:36 -0400

    Add cling to the correct wsgi file

--- tributary/wsgi.py
+++ tributary/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tributary.settings")
 
 application = get_wsgi_application()
+
+# Apply WSGI middleware here.
+from dj_static import Cling
+application = Cling(application)
