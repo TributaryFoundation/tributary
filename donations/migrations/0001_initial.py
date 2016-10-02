4230d8fbb337250891eb8d4c1207166c2c827c04
Spencer Nelson <s@spenczar.com>
2016-10-02 15:48:59 -0400

    Regenerate migrations so they work on postgres

--- donations/migrations/0001_initial.py
+++ donations/migrations/0001_initial.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+import uuid
 
 
 class Migration(migrations.Migration):
@@ -13,11 +14,11 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Donation',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
-                ('email_address', models.EmailField(help_text='Email address of the donor who made this donation.', max_length=254)),
+                ('id', models.UUIDField(default=uuid.uuid4, primary_key=True, serialize=False, editable=False)),
+                ('email_address', models.EmailField(max_length=254, help_text='Email address of the donor who made this donation.')),
                 ('monthly_amount', models.PositiveSmallIntegerField(help_text='Amount to donate each month in whole dollars.')),
                 ('instructions', models.TextField(help_text='The unstructured instructions describing where the donation should be sent.')),
-                ('donor_name', models.CharField(help_text='When making a donation, do it on behalf of this name.', max_length=1000)),
+                ('donor_name', models.CharField(max_length=1000, help_text='When making a donation, do it on behalf of this name.')),
             ],
         ),
     ]
